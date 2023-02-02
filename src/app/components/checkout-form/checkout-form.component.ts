d212744d9da0a78e99c15608fdf4130c4f38809a
Hektor-Byfors <hektor.byfors@hotmail.com>
2023-02-02 16:25:13 +0100

    order structure from checkout form

--- src/app/components/checkout-form/checkout-form.component.ts
+++ src/app/components/checkout-form/checkout-form.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-checkout-form',
@@ -8,6 +9,12 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./checkout-form.component.scss']
 })
 export class CheckoutFormComponent implements OnInit {
+  @Input() cartProducts!: product[];
+
+  totalPrice: number = 0;
+
+  orderRows: string[] = [];
+
   checkoutForm = new FormGroup({
     name: new FormControl(""),
     payment: new FormControl("")
@@ -16,6 +23,10 @@ export class CheckoutFormComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.cartProducts.forEach((p) => {
+      this.totalPrice += p.price;
+      this.orderRows.push(p.name)
+    })
   }
 
   apiPostUrl: string = "https://medieinstitutet-wie-products.azurewebsites.net/api/orders";
@@ -23,17 +34,49 @@ export class CheckoutFormComponent implements OnInit {
   order = {
     companyId: 990218,
     created: new Date(),
-    createdBy: "test subject",
-    paymentMethod: "Swish",
-    totalPrice: 100,
+    createdBy: this.checkoutForm.value.name,
+    paymentMethod: this.checkoutForm.value.payment,
+    totalPrice: this.totalPrice,
     status: 0,
     orderRows: []
   }
 
   onSubmit(){
-    console.log(this.checkoutForm.value);
-    /* this.http.post(this.apiPostUrl, this.order).subscribe((res) => {
+    /* console.log(this.order);
+    console.log({
+      companyId: 990218,
+      created: new Date(),
+      createdBy: this.checkoutForm.value.name,
+      paymentMethod: this.checkoutForm.value.payment,
+      totalPrice: this.totalPrice,
+      status: 0,
+      orderRows: this.orderRows
+    });
+    
+    console.log(this.totalPrice);
+    
+    
+    console.log(this.checkoutForm.value.name);
+    console.log(this.cartProducts); */
+    
+    this.http.post(this.apiPostUrl, 
+      {
+        companyId: 990218,
+        created: new Date(),
+        createdBy: this.checkoutForm.value.name,
+        paymentMethod: this.checkoutForm.value.payment,
+        totalPrice: this.totalPrice,
+        status: 0,
+        orderRows: [
+          {
+            productId: 76,
+            product: null,
+            amount: 2
+          }
+        ]
+      })
+      .subscribe((res) => {
       console.log(res);
-    }) */
+    })
   }
 }
